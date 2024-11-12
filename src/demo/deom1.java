37cbe188cf7f58e4b62e4ffe1dde91e5ebfdfe02
Rajavignesh <rvthara96@gmail.com>
2024-11-12 16:15:31 +0530

    demo commit

---
+++ src/demo/deom1.java
@@ -0,0 +1,14 @@
+package demo;
+
+public class deom1 {
+	public static void main(String[] args) {
+
+		int a = 10;
+
+		if (a == 10) {
+			System.out.println("true");
+		} else
+			System.out.println("False");
+	}
+
+}
